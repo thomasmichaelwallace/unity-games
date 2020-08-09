2c6342410fd7bc6c1090571e44ca10811e1be9fb
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-09 21:49:19 +0100

    feat(pilot): too simple dialouge system

--- pilot/Assets/Scripts/DialogueBehaviour.cs
+++ pilot/Assets/Scripts/DialogueBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogManager : MonoBehaviour
+public class DialogueBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
