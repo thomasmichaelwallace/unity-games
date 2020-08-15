965b42ec16b056cb7593a6da4047151ef98ad1cb
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-15 15:20:49 +0100

    feat(crabs): initial prototype code

---
+++ crabs/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Material Hightlight;
+
+    private float health = 3f;
+    private bool tookDamage = false;
+    private Material defaultMaterial;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultMaterial = meshRenderer.material;
+    }
+
+    private void Update()
+    {
+        if (tookDamage)
+        {
+            tookDamage = false;
+            meshRenderer.material = Hightlight;
+        }
+        else
+        {
+            meshRenderer.material = defaultMaterial;
+        }
+    }
+
+    public void TakeDamage(float strength)
+    {
+        tookDamage = true;
+        health -= strength * Time.deltaTime;
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ crabs/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public MeshRenderer[] Claws;
+    public Material Hightlight;
+
+    private float speed = 10f;
+    private float lookSpeed = 20f;
+    private float attackDistance = 10f;
+    private float strength = 1f;
+
+    private CharacterController characterController;
+    private Material clawMaterial;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        clawMaterial = Claws[0].material;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            float hoizontal = Input.GetAxis("Horizontal");
+            Vector3 rotation = new Vector3(0, hoizontal, 0) * lookSpeed;
+            transform.eulerAngles += (rotation * Time.deltaTime);
+        }
+        else
+        {
+            float hoizontal = Input.GetAxis("Horizontal");
+            Vector3 movement = transform.TransformDirection(Vector3.right) * hoizontal * speed;
+            characterController.Move(movement * Time.deltaTime);
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            foreach (MeshRenderer claw in Claws)
+            {
+                claw.material = Hightlight;
+            }
+
+            // Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward) * attackDistance);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, attackDistance))
+            {
+                var enemy = hit.transform.GetComponent<EnemyController>();
+                if (enemy)
+                {
+                    enemy.TakeDamage(strength);
+                }
+            }
+        }
+        else
+        {
+            foreach (MeshRenderer claw in Claws)
+            {
+                claw.material = clawMaterial;
+            }
+        }
+    }
+}
