642949de222fefbdbd20072cee82cfbb216f1c59
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-16 14:25:46 +0100

    feat(crabs): random spawning

--- crabs/Assets/Scripts/EnemyController.cs
+++ crabs/Assets/Scripts/EnemyController.cs
@@ -14,18 +14,39 @@ public class EnemyController : MonoBehaviour
     private Material[] defaultMaterials;
     private MeshRenderer[] meshRenderers;
     private NavMeshAgent agent;
+    private Rigidbody rb;
 
     private void Start()
     {
         meshRenderers = GetComponentsInChildren<MeshRenderer>();
         defaultMaterials = meshRenderers.Select(m => m.material).ToArray();
         agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        rb.velocity = Vector3.down;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!agent.enabled)
+        {
+            if (Mathf.Approximately(rb.velocity.y, 0f))
+            {
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                agent.enabled = true;
+            }
+        }
     }
 
     private void Update()
     {
-        agent.SetDestination(Player.position);
-
+        if (agent.isActiveAndEnabled)
+        {
+            agent.SetDestination(Player.position);
+        }
 
         if (tookDamage)
         {
---
+++ crabs/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public Transform Player;
+    public GameObject Prefab;
+
+    private readonly float interval = 10f;
+    private readonly float fieldSize = 25f;
+    private float timer = 0f;
+    
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = interval;
+            Debug.Log("spawning...");
+            Vector3 position = UnityEngine.Random.insideUnitSphere * fieldSize;
+            position.y = fieldSize;
+            GameObject enemy = Instantiate(Prefab, position, Quaternion.identity);
+            enemy.transform.SetParent(transform);
+            EnemyController controller = enemy.GetComponent<EnemyController>();
+            controller.Player = Player;
+        }
+    }
+}
