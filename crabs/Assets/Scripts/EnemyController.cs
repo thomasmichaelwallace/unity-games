91ccb7c79266e17c86e411727b2b2a824aefd24e
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-16 11:47:36 +0100

    fix(crabs): enemies now follow mesh

--- crabs/Assets/Scripts/EnemyController.cs
+++ crabs/Assets/Scripts/EnemyController.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public Transform Player;
     public Material Hightlight;
 
     private float health = 3f;
     private bool tookDamage = false;
     private Material[] defaultMaterials;
     private MeshRenderer[] meshRenderers;
+    private NavMeshAgent agent;
 
     private void Start()
     {
         meshRenderers = GetComponentsInChildren<MeshRenderer>();
         defaultMaterials = meshRenderers.Select(m => m.material).ToArray();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
+        agent.SetDestination(Player.position);
+
+
         if (tookDamage)
         {
             tookDamage = false;
