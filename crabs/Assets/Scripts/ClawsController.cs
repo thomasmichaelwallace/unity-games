b99ac2946f25a7afa9821a633f688b5d9790335b
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-17 21:32:20 +0100

    fix(crabs): wider attack area

--- crabs/Assets/Scripts/ClawsController.cs
+++ crabs/Assets/Scripts/ClawsController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ClawsController : MonoBehaviour
 {
-    private readonly float attackDistance = 3f;
+    private readonly float attackDistance = 1.5f;
+    private readonly float attackRadius = 1f;
     private readonly float strength = 1f;
-    private readonly float hitForce = 1f;
 
     private Animator[] animators;
     private bool isAttacking;
@@ -19,8 +19,6 @@ public class ClawsController : MonoBehaviour
 
     private void Update()
     {
-        // TODO: replace "hightlight" with attack animation
-        // OPTION: add more interesting left vs right combat
         bool fireButtonDown = Input.GetButton("Fire1");
         if (fireButtonDown && !isAttacking)
         {
@@ -42,13 +40,15 @@ public class ClawsController : MonoBehaviour
         if (isAttacking)
         {
             Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward) * attackDistance);
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, attackDistance))
+            Debug.DrawLine(transform.position + transform.TransformDirection(Vector3.left) * attackRadius, transform.position + transform.TransformDirection(Vector3.left) * attackRadius  + transform.TransformDirection(Vector3.forward) * attackDistance);
+            Debug.DrawLine(transform.position + transform.TransformDirection(Vector3.right) * attackRadius, transform.position + transform.TransformDirection(Vector3.right) * attackRadius + transform.TransformDirection(Vector3.forward) * attackDistance);
+            RaycastHit hit;
+            if (
+                Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, attackDistance)
+                || Physics.Raycast(transform.position + transform.TransformDirection(Vector3.left) * attackRadius, transform.TransformDirection(Vector3.forward), out hit, attackDistance)
+                || Physics.Raycast(transform.position + transform.TransformDirection(Vector3.right) * attackRadius, transform.TransformDirection(Vector3.forward), out hit, attackDistance)
+            )
             {
-                if (hit.rigidbody)
-                {
-                    Vector3 direction = hit.transform.position - transform.position;
-                    hit.rigidbody.AddForce(direction * hitForce);
-                }
                 EnemyController enemy = hit.transform.GetComponent<EnemyController>();
                 if (enemy)
                 {
