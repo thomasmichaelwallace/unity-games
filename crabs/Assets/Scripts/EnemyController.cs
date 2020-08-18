d6bca345d5febbe346d0014eeafbd8e3a4a2ccbf
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-18 14:53:41 +0100

    fix(crabs): attack in-front

--- crabs/Assets/Scripts/EnemyController.cs
+++ crabs/Assets/Scripts/EnemyController.cs
@@ -15,10 +15,13 @@ public class EnemyController : MonoBehaviour
     private readonly float gravity = 10f;
     private readonly float turnAllowance = 5f;
     private readonly float turnSpeed = 1f;
-    private readonly float attackDistance = 3f;
     private readonly float initalHeath = 1f;
     private readonly float allowableDistance = 0.5f;
     private readonly float checkInterval = 2.0f;
+    private readonly float attackDistance = 2.0f;
+    private readonly float attackRadius = 1f;
+    private readonly float strength = 1f;
+    private readonly float startAttackDistance = 3f;
 
     private float health;
     private bool tookDamage = false;
@@ -44,6 +47,13 @@ public class EnemyController : MonoBehaviour
         player = Player.GetComponent<PlayerController>();
     }
 
+    // Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, attackDistance)
+    private void DebugRayLine(Vector3 position, Vector3 direction, out RaycastHit hit, float distance)
+    {
+        hit = new RaycastHit(); // has to be set.
+        Debug.DrawLine(position, position + direction * distance);
+    }
+
     private void Update()
     {
         if (isDead) return;
@@ -64,16 +74,17 @@ public class EnemyController : MonoBehaviour
                     transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - distance, transform.localPosition.z);
                 }
             }
-            else if (isTurning)
-            {
-                Vector3 rotation = Vector3.RotateTowards(transform.forward, turnDirection, turnSpeed * Time.deltaTime, 0.0f);
-                transform.localRotation = Quaternion.LookRotation(rotation);
-                if (Vector3.Angle(transform.forward, agent.desiredVelocity) < turnAllowance)
-                {
-                    isTurning = false;
-                    agent.enabled = true;
-                }
-            }
+            //else if (isTurning)
+            //{
+            //    Vector3 rotation = Vector3.RotateTowards(transform.forward, turnDirection, turnSpeed * Time.deltaTime, 0.0f);
+            //    transform.localRotation = Quaternion.LookRotation(rotation);
+            //    if (Vector3.Angle(transform.forward, agent.desiredVelocity) < turnAllowance)
+            //    {
+            //        isTurning = false;
+            //        agent.enabled = true;
+            //        agent.SetDestination(Player.position);
+            //    }
+            //}
         }
         else
         {
@@ -95,32 +106,59 @@ public class EnemyController : MonoBehaviour
                     Die(); // probably landed on a tree :/
                 }
 
-                if (agent.remainingDistance > 0 && agent.remainingDistance < attackDistance)
+                if (agent.remainingDistance < startAttackDistance)
                 {
+                    if (true)
+                    {
+                        RaycastHit hit;
+                        DebugRayLine(transform.position, transform.TransformDirection(Vector3.right), out hit, attackDistance);
+                        DebugRayLine(transform.position + transform.TransformDirection(Vector3.forward) * attackRadius, transform.TransformDirection(Vector3.right), out hit, attackDistance);
+                        DebugRayLine(transform.position + transform.TransformDirection(Vector3.back) * attackRadius, transform.TransformDirection(Vector3.right), out hit, attackDistance);
+
+                        if (
+                            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, attackDistance)
+                            || Physics.Raycast(transform.position + transform.TransformDirection(Vector3.forward) * attackRadius, transform.TransformDirection(Vector3.right), out hit, attackDistance)
+                            || Physics.Raycast(transform.position + transform.TransformDirection(Vector3.back) * attackRadius, transform.TransformDirection(Vector3.right), out hit, attackDistance)
+                        )
+                        {
+                            PlayerController player = hit.transform.GetComponent<PlayerController>();
+                            if (player)
+                            {
+                                player.DoDamage();
+                            }
+                        }
+                    }
                     // start attacking!
-                    player.DoDamage();
-                    agent.SetDestination(transform.position);
+                    //player.DoDamage();
+                    //agent.SetDestination(transform.position);
                 }
                 else
                 {
                     Vector3 targetAngle = new Vector3(agent.desiredVelocity.x, 0, agent.desiredVelocity.z);
                     if (Vector3.Angle(transform.forward, targetAngle) > turnAllowance)
                     {
-                        isTurning = true;
-                        turnDirection = agent.desiredVelocity;
-                        agent.enabled = false;
+                        agent.speed = 0;
+                        agent.angularSpeed = 120f;
+                        // isTurning = true;
+                        // turnDirection = agent.desiredVelocity;
+                        // agent.enabled = false;
                     }
                     else
                     {
-                        // stick to ground
-                        Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f);
-                        var target = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.right, hit.normal), hit.normal);
-                        Crab.rotation = Quaternion.RotateTowards(Crab.rotation, target, 1f);
-                        if (hit.distance > allowableDistance)
-                        {
-                            Crab.transform.position = Vector3.MoveTowards(Crab.transform.position, hit.point, Time.deltaTime);
-                        }
+                        agent.speed = 3.5f;
+                        agent.angularSpeed = 0f;
                     }
+                    //else
+                    //{
+                    // stick to ground
+                    Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f);
+                    var target = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.right, hit.normal), hit.normal);
+                    Crab.rotation = Quaternion.RotateTowards(Crab.rotation, target, 1f);
+                    if (hit.distance > allowableDistance)
+                    {
+                        Crab.transform.position = Vector3.MoveTowards(Crab.transform.position, hit.point, Time.deltaTime);
+                    }
+                    //}
                 }
             }
         }
