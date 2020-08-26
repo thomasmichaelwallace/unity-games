62e1ddc0152425e967d937ae9b4bfb741734f2b1
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-26 16:03:29 +0100

    feat(tycoon): raising water

---
+++ tycoon/Assets/Scripts/Water.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public float Speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var delta = Speed * Time.deltaTime; 
+        
+        var scale = transform.localScale;
+        scale.y += delta;
+        transform.localScale = scale;
+
+        var position = transform.localPosition;
+        position.y += (delta / 2f);
+        transform.localPosition = position;
+    }
+}
