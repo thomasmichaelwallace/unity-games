5cb85778a3b9b4f5ac7f1413aa65cefe56740e44
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-30 15:59:20 +0100

    feat(hurl): grab and shoot ball

--- hurl/Assets/Scripts/PlayerController.cs
+++ hurl/Assets/Scripts/PlayerController.cs
@@ -1,37 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody fieldBall;
+    public GameObject playerBall;
+    private readonly float _angle = 0.5f; // raise at 2:1
+    private readonly float _effortRate = 2f;
+    private readonly float _maxEffort = 4f;
 
-    public Rigidbody ball;
+    private readonly float _speed = 2.5f;
+    private readonly float _strength = 10f;
+    private readonly Vector3 _ballOffset = new Vector3(0f,0.15f, -0.5f);
 
-    private readonly float speed = 2.5f;
-    private readonly float strength = 5f;
-    private readonly float angle = 0.5f; // raise at 2:1
-    
-    // Start is called before the first frame update
-    void Start()
+    private float _effort;
+    private bool _hasBall = false;
+
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if (other.rigidbody == fieldBall)
+        {
+            _hasBall = true;
+            fieldBall.isKinematic = true;
+            fieldBall.constraints = RigidbodyConstraints.None;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
+        if (_hasBall)
+        {
+            fieldBall.position = transform.position + _ballOffset;
+        }
+    }
 
-        Vector3 position = transform.position;
-        position.x += horizontal * speed * Time.deltaTime;
-        position.z += vertical * speed * Time.deltaTime;
+    private void Update()
+    {
+        // movement
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        var position = transform.position;
+        position.x += horizontal * _speed * Time.deltaTime;
+        position.z += vertical * _speed * Time.deltaTime;
         transform.position = position;
         
-        if (Input.GetButtonDown("Fire1"))
+        // hit
+        if (_hasBall)
         {
-            Vector3 impact = new Vector3(0, angle * strength, -strength);
-            ball.AddForce(impact, ForceMode.VelocityChange);
+            if (Input.GetButton("Fire1"))
+            {
+                _effort += _effortRate * Time.deltaTime;
+                _effort = Mathf.Min(_effort, _maxEffort);
+            }
+            else if (_effort > 0)
+            {
+                fieldBall.isKinematic = false;
+                fieldBall.constraints = RigidbodyConstraints.FreezePositionX;
+                _hasBall = false;
+                
+                var impact = new Vector3(0, _angle * _strength * _effort, -_strength * _effort);
+                fieldBall.AddForce(impact); // , ForceMode.VelocityChange);
+
+                _effort = 0;
+            }
+            else
+            {
+                _effort = 0;
+            }
         }
     }
 }
