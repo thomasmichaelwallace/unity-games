52c01260798fa72918d03983e0188a182cc1835b
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-30 21:33:19 +0100

    feat(hurl): goal keeping

---
+++ hurl/Assets/Scripts/GoalBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehaviour : MonoBehaviour
+{
+    public int points;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log($"Score! +{points}");
+        }
+    }
+
+}
