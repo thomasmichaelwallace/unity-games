fb422fa740c0ea553b4ca08fe695a93fac9af302
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-08-30 14:52:17 +0100

    feat(hurl): player/ball interaction

---
+++ hurl/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody ball;
+
+    private readonly float speed = 2.5f;
+    private readonly float strength = 5f;
+    private readonly float angle = 0.5f; // raise at 2:1
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        position.z += vertical * speed * Time.deltaTime;
+        transform.position = position;
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 impact = new Vector3(0, angle * strength, -strength);
+            ball.AddForce(impact, ForceMode.VelocityChange);
+        }
+    }
+}
