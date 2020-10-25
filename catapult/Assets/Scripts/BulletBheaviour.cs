9219cf6ae79fd8cafe7416ad25bc8c04d73bed77
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-10-25 16:25:53 +0000

    feat(catapult): bullet time button

---
+++ catapult/Assets/Scripts/BulletBheaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BulletBheaviour : MonoBehaviour
+{
+    private bool _isSlowing = false;
+    
+    private float _fullSpeed;
+    private float _slowSpeed = 0.1f;
+    
+    void Start()
+    {
+        _fullSpeed = Time.timeScale;
+    }
+    
+    void Update()
+    {
+        if (_isSlowing)
+        {
+            Time.timeScale = _slowSpeed;
+        }
+        else
+        {
+            Time.timeScale = _fullSpeed;
+        }
+    }
+
+    public void SlowInput(InputAction.CallbackContext context)
+    {
+        _isSlowing = context.ReadValueAsButton();
+    }
+}
