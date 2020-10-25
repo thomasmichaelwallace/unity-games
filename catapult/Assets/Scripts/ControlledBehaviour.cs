738ec7f8e246b5c989460873f934e4493755b125
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-10-25 18:02:37 +0000

    feat(catapult): ball tweening

--- catapult/Assets/Scripts/ControlledBehaviour.cs
+++ catapult/Assets/Scripts/ControlledBehaviour.cs
@@ -5,13 +5,50 @@ using UnityEngine.InputSystem;
 public class ControlledBehaviour : MonoBehaviour
 {
     private int _target = 0;
+    
     private float _to = 0;
+    private float _velocity = 0;
+    private float _acceleration = 5f;
     
     void Update()
     {
-        Vector3 p = transform.position;
-        p.x = _to;
-        transform.position = p;
+        Debug.Log(_velocity);
+        
+        var x = transform.position.x;
+        if (Mathf.Approximately(_to, x))
+        {
+            _velocity = 0;
+            return;
+        }
+
+        float delta = Mathf.Abs(x - _to);
+        float snap = _acceleration * Time.deltaTime;
+        if (delta < snap && Mathf.Abs(_velocity) < _acceleration)
+        {
+            // do snap
+            transform.position = new Vector3(_to, 0, 0f);
+            return;
+        }
+
+        int sign = 0;
+        float stopping =Mathf.Pow(_velocity, 2f) / (2 * _acceleration);        
+        if (delta > stopping)
+        {
+            sign = 1; // accelerate
+        }
+        else
+        {
+            sign = -1;
+        }
+        
+        if (x > _to)
+        {
+            sign *= -1; // heading right
+        }
+        _velocity += sign * snap;
+        x += _velocity * Time.deltaTime;
+        
+        transform.position = new Vector3(x, 0, 0);
     }
 
     private void Move(int direction)
@@ -20,6 +57,8 @@ public class ControlledBehaviour : MonoBehaviour
         if (_target < -1) _target = -1;
         if (_target > 1) _target = 1;
         _to = _target * 2f;
+        
+        Debug.Log(_to);
     }
     
     public void Left(InputAction.CallbackContext context)
