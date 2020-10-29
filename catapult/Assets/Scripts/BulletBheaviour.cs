87c58516b0dbcf2df8f55fd6431a5097142c7370
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-10-29 10:37:50 +0000

    feat(catapult): bullet time tween

--- catapult/Assets/Scripts/BulletBheaviour.cs
+++ catapult/Assets/Scripts/BulletBheaviour.cs
@@ -6,6 +6,8 @@ public class BulletBheaviour : MonoBehaviour
     private float _fullSpeed;
     private bool _isSlowing;
     private readonly float _slowSpeed = 0.1f;
+    private float _slowRate = 2f;
+    private float _fastRate = 5f;
 
     private void Start()
     {
@@ -14,10 +16,13 @@ public class BulletBheaviour : MonoBehaviour
 
     private void Update()
     {
-        if (_isSlowing)
-            Time.timeScale = _slowSpeed;
-        else
-            Time.timeScale = _fullSpeed;
+        if (_isSlowing && Time.timeScale > _slowSpeed)
+        {
+            Time.timeScale -= _slowRate * Time.deltaTime;
+        } else if (!_isSlowing && Time.timeScale < _fullSpeed)
+        {
+            Time.timeScale += _fastRate * Time.deltaTime;
+        }
     }
 
     public void SlowInput(InputAction.CallbackContext context)
