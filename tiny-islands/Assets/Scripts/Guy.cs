e30bcb46bf41789f4f3b1b195f6f820f097e1428
thomas michael wallace <thomasmichaelwallace@gmail.com>
2021-04-08 15:48:25 +0100

    refactor(tiny-island): simply guy behaviour

--- tiny-islands/Assets/Scripts/Guy.cs
+++ tiny-islands/Assets/Scripts/Guy.cs
@@ -13,13 +13,10 @@ public class Guy : MonoBehaviour
 
     private Quaternion _deadAngle;
     private float _drownSpeed;
-    private bool _isDrowning;
-    private bool _isDying;
-    private bool _isJumping;
-    private bool _isSaved;
     private float _sqrBoatLength;
     private float _sqrJumpLength;
-    private float _yVelocity;
+    private State _state = State.Waiting;
+    private float _jumpVelocity;
 
     private void Awake()
     {
@@ -32,78 +29,88 @@ public class Guy : MonoBehaviour
         _drownSpeed = 90f / drownTime;
     }
 
+
     private void Update()
     {
-        if (_isSaved) return;
+        if (_state == State.Saved || _state == State.Dead) return;
 
         var t = transform;
         var position = t.position;
 
-        if (_isJumping)
+        // can be saved
+        if (_state == State.Waiting || _state == State.Drowning)
+        {
+            var distance = (position - boat.position).sqrMagnitude;
+            if (distance < _sqrJumpLength)
+            {
+                _jumpVelocity = jumpHeight;
+                _state = State.Jumping;
+            }
+        }
+
+        // sinking (either on land, or after drowning)
+        if (_state == State.Waiting || _state == State.Sinking)
+        {
+            position.y -= Water.RisingSpeed * Time.deltaTime;
+            t.position = position;
+
+            if (position.y <= 0f && _state == State.Waiting)
+            {
+                _state = State.Drowning;
+                _sqrJumpLength = 1f;
+            }
+            else if (position.y <= SinkPoint && _state == State.Sinking)
+            {
+                _state = State.Dead;
+                Destroy(gameObject);
+            }
+        }
+
+        // drowning
+        if (_state == State.Drowning)
+        {
+            var rotation = t.rotation;
+            rotation = Quaternion.RotateTowards(rotation, _deadAngle, _drownSpeed * Time.deltaTime);
+            t.rotation = rotation;
+
+            if (Quaternion.Angle(rotation, _deadAngle) <= 0.01f) _state = State.Sinking;
+        }
+
+        // jumping
+        if (_state == State.Jumping)
         {
             var current = new Vector3(position.x, 0, position.z);
             var target = boat.position;
             target.y = 0;
 
             current = Vector3.MoveTowards(current, target, jumpSpeed * Time.deltaTime);
-            _yVelocity -= 9.81f * Time.deltaTime;
-            current.y = position.y + _yVelocity * Time.deltaTime;
+            _jumpVelocity -= 9.81f * Time.deltaTime;
+            current.y = position.y + _jumpVelocity * Time.deltaTime;
 
             if (current.y <= 0f)
             {
-                _isDrowning = true;
-                _isJumping = false;
-                _sqrJumpLength = 1f;
+                _state = State.Drowning;
                 current.y = 0;
             }
 
-            var distance = (position - boat.position).sqrMagnitude;
+            var distance = (current - boat.position).sqrMagnitude;
             if (distance < _sqrBoatLength)
             {
-                _isSaved = true;
+                _state = State.Saved;
                 Destroy(gameObject);
             }
 
             t.position = current;
-            return;
         }
+    }
 
-        // waiting to be saved
-        if (!_isDying)
-        {
-            var distance = (position - boat.position).sqrMagnitude;
-            if (distance < _sqrJumpLength)
-            {
-                _yVelocity = jumpHeight;
-                _isJumping = true;
-                return;
-            }
-        }
-
-        // drowning
-        if (_isDrowning)
-        {
-            var rotation = t.rotation;
-            rotation = Quaternion.RotateTowards(rotation, _deadAngle, _drownSpeed * Time.deltaTime);
-            t.rotation = rotation;
-            if (!Mathf.Approximately(rotation.eulerAngles.x, 90f)) return;
-
-            _isDying = true;
-            _isDrowning = false;
-            return;
-        }
-
-        // sinking
-        position.y -= Water.RisingSpeed * Time.deltaTime;
-        t.position = position;
-        if (position.y <= 0f && !_isDying)
-        {
-            _isDrowning = true;
-            _sqrJumpLength = 1f;
-        }
-        else if (position.y <= SinkPoint && _isDying)
-        {
-            Destroy(gameObject);
-        }
+    private enum State
+    {
+        Waiting,
+        Jumping,
+        Saved,
+        Drowning,
+        Sinking,
+        Dead
     }
 }
